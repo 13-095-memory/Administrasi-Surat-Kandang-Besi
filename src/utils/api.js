96d94945cf7b0292a874c76b45ebaa570027f1a5
01-123140180-MegaZayyani <megazayyani115@gmail.com>
2026-01-30 01:38:03 +0700

    ubah API untuk nyambung ke backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,29 +1,22 @@
 // src/utils/api.js
-const api = {
-  get: async (url) => {
-    console.log("Mock GET request to:", url);
-    return {
-      data: {
-        user: {
-          firstName: "Budi",
-          lastName: "Santoso",
-          nik: "3201234567890123",
-          tempatLahir: "Bandar Lampung",
-          tanggalLahir: "1990-01-01",
-          jenisKelamin: "Laki-laki",
-          agama: "Islam",
-          statusPerkawinan: "Kawin",
-          pekerjaan: "Wiraswasta",
-          kewarganegaraan: "Indonesia",
-          phoneNumber: "08123456789"
-        }
-      }
-    };
+import axios from 'axios';
+
+const API_URL = 'https://backend-administrasi-surat-kandang-two.vercel.app/'; 
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
   },
-  post: async (url, data) => {
-    console.log("Mock POST request to:", url, data);
-    return { data: { surat: { noTiket: "TKT-" + Date.now() } } };
+});
+
+// Tambahkan interceptor untuk menangani token jika ada (opsional)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-};
+  return config;
+});
 
 export default api;
